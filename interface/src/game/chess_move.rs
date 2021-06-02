30d7bd905acbb3867b4987832235ef8f7254cd47
Jeffrey Meyer <jam93t@mst.edu>
2021-06-01 22:22:24 -0500

    Add doc-comments to ChessMove and Piece traits and fix from_simple_move
    ChessMove now has an associated Board type to use with from_simple_move
    which should now attempt to parse move legality and side-effects from a
    board.

--- interface/src/game/chess_move.rs
+++ interface/src/game/chess_move.rs
@@ -1,34 +1,60 @@
 use std::fmt::{Debug, Display};
 use crate::game::Position;
+use crate::engine::Board;
 
+/// Basic mve with only the origin and destination
 pub trait SimpleChessMove<P: Position>: Display + Debug + Sized + Clone {
+    /// Create a simple move going from one square to another
     fn new(from: P::Square, to: P::Square) -> Self;
+    /// Starting square
     fn from(&self) -> P::Square;
+    /// Ending square
     fn to(&self) -> P::Square;
 }
 
+/// Move with information on its side-effects (thus can be applied to a board)
 pub trait ChessMove<P: Position>: SimpleChessMove<P> {
+    /// Type for simple move to inherit from
     type SimpleChessMove: SimpleChessMove<P>;
+    /// Type for board representation to pull move information from
+    type Board: Board<P>;
 
+    /// Special const to avoid re-constructing multiple white king castles
     const WHITE_KING_CASTLE: Self;
+    /// Special const to avoid re-constructing multiple white queen castles
     const WHITE_QUEEN_CASTLE: Self;
+    /// Special const to avoid re-constructing multiple black king castles
     const BLACK_KING_CASTLE: Self;
+    /// Special const to avoid re-constructing multiple black queen castles
     const BLACK_QUEEN_CASTLE: Self;
 
+    /// Create a new double pawn push move
     fn new_double_pawn_push(from: P::Square, to: P::Square) -> Self;
+    /// Create a new en passant pawn capture move
     fn new_en_passant_capture(from: P::Square, to: P::Square) -> Self;
+    /// Create a new capture move
     fn new_capture(from: P::Square, to: P::Square) -> Self;
+    /// Create a new pawn promotion move
     fn new_promotion(from: P::Square, to: P::Square, promotion: P::Piece) -> Self;
+    /// Create a new pawn promotion and capture move
     fn new_promoting_capture(from: P::Square, to: P::Square, promotion: P::Piece) -> Self;
 
-    fn from_simple_move(simple_move: Self::SimpleChessMove) -> Self;
-    fn simple_move(&self) -> Self::SimpleChessMove;
+    /// Make a chess move from a simple move (determine what side-effects a move has (ie. the move captures or leaves an en-passant square))
+    fn from_simple_move(simple_move: Self::SimpleChessMove, board: &Self::Board) -> Self;
+    /// Get the promotion piece (empty if non-promoting move)
     fn promotion(&self) -> P::Piece;
+    /// If a move is has no side-effects (not a capture, double pawn push, castle, or promotion)
     fn is_quiet(&self) -> bool;
+    /// If a move is a double pawn push
     fn is_double_pawn_push(&self) -> bool;
+    /// If a move is a promotion
     fn is_promotion(&self) -> bool;
+    /// If a move is a capture
     fn is_capture(&self) -> bool;
+    /// If a move is a king-side castle
     fn is_king_castle(&self) -> bool;
+    /// If a move is a queen-side castle
     fn is_queen_castle(&self) -> bool;
+    /// If a move is an en-passant capture
     fn is_en_passant_capture(&self) -> bool;
 }
--- interface/src/game/piece.rs
+++ interface/src/game/piece.rs
@@ -1,35 +1,62 @@
 use crate::game::Position;
 use std::fmt::{Debug, Display};
 
+/// A board piece without side (ie. rook, bishop, queen, etc)
 pub trait Piece<P: Position>: Sized + Debug + Display + Default + Eq + PartialEq + From<char> {
+    /// List of pieces (excluding empty) for iteration
     const PIECES: [Self; 6];
+    /// Piece representing a pawn
     const PAWN: Self;
+    /// Piece representing a knight
     const KNIGHT: Self;
+    /// Piece representing a bishop
     const BISHOP: Self;
+    /// Piece representing a rook
     const ROOK: Self;
+    /// Piece representing a queen
     const QUEEN: Self;
+    /// Piece representing a king
     const KING: Self;
+    /// Piece representing an empty square
     const EMPTY: Self;
 
+    /// Get the sided piece version of a piece for a given side
     fn add_side(self, side: P::Side) -> P::SidedPiece;
 }
 
+/// A side's piece (ie. white rook)
 pub trait SidedPiece<P: Position>: Sized + Debug + Display + Default + Eq + PartialEq + From<char> + Into<char> {
+    /// List of aLl pieces (excluding empty) for iteration
     const PIECES: [Self; 12];
+    /// Piece representing a white pawn
     const WHITE_PAWN: Self;
+    /// Piece representing a black pawn
     const BLACK_PAWN: Self;
+    /// Piece representing a white knight
     const WHITE_KNIGHT: Self;
+    /// Piece representing a black knight
     const BLACK_KNIGHT: Self;
+    /// Piece representing a white bishop
     const WHITE_BISHOP: Self;
+    /// Piece representing a black bishop
     const BLACK_BISHOP: Self;
+    /// Piece representing a white rook
     const WHITE_ROOK: Self;
+    /// Piece representing a black rook
     const BLACK_ROOK: Self;
+    /// Piece representing a white queen
     const WHITE_QUEEN: Self;
+    /// Piece representing a black queen
     const BLACK_QUEEN: Self;
+    /// Piece for white's king
     const WHITE_KING: Self;
+    /// Piece for black's king
     const BLACK_KING: Self;
+    /// Piece for no piece (empty square on board)
     const EMPTY: Self;
 
+    /// Get the side of a sided piece
     fn side(&self) -> P::Side;
+    /// Get the unsided-piece (ie white-pawn -> pawn, black-rook -> rook)
     fn unsided_piece(&self) -> P::Piece;
 }
